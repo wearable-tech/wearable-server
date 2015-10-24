3003d8c7c107da9c7b4e4717dd13bcae1da14a5a
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2015-10-24 15:15:55 -0200

    Name field user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require "./arnorails.rb"
 
-User.create(email: "admin1", password: "admin1")
-User.create(email: "admin2", password: "admin2")
-User.create(email: "admin3", password: "admin3")
+User.create(name: "admin", email: "admin@a.com", password: "admin")
+User.create(name: "teste", email: "teste@a.com", password: "teste")
+User.create(name: "root", email: "root@a.com", password: "root")
+User.create(name: "usuario", email: "usuario@a.com", password: "usuario")
