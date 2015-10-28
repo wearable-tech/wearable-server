008bc07d3a9b92a851399902bc628dabe29ceeb1
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2015-10-28 15:26:58 -0200

    Publish to contacts!

--- app/controllers/mqtt.rb
+++ app/controllers/mqtt.rb
@@ -31,13 +31,33 @@ def save_location(user, latitude, longitude)
   Geocoder.search("#{latitude},#{longitude}").first.address
 end
 
+def send_notification(email, level, message)
+    for i in level..3
+      MQTT::Client.connect(connection) do |c|
+        puts "to_#{email}_#{i}: #{message}"
+        c.publish("to_#{email}_#{i}", message)
+      end
+    end
+end
+
+def verify_notification(user, measurement)
+  if measurement.oxygen_level > 0
+    send_notification(user.email, measurement.oxygen_level,
+        "O nível oxigenação do(a) #{user.name} é #{measurement.blood_oxygenation}")
+  end
+  if measurement.pulse_level > 0
+    send_notification(user.email, measurement.pulse_level,
+        "A frequência cardíaca do(a) #{user.name} é #{measurement.pulse_rate}")
+  end
+end
+
 def init_subscribe(email)
   user = User.find_by_email email
 
   Thread.new do
     puts "Doing subscribe to #{email}"
     MQTT::Client.connect(connection) do |c|
-      c.get(email) do |topic, message|
+      c.get("from_" + email) do |topic, message|
         puts "#{topic}: #{message}"
 
         params = message.split(",")
@@ -46,33 +66,24 @@ def init_subscribe(email)
         oxygenation = params[2].to_f
         pulse = params[3].to_f
 
-        Measurement.create(user_id: user.id, blood_oxygenation: oxygenation, pulse_rate: pulse)
+        measurement = Measurement.create(user_id: user.id, blood_oxygenation: oxygenation,
+          pulse_rate: pulse)
+
         puts save_location(user, latitude, longitude)
+
+        verify_notification(user, measurement)
       end
     end
   end
 end
 
 def init_connection
-  Thread.new do
-    MQTT::Client.connect(connection) do |c|
-      c.get("new_connection") do |topic, message|
-        puts "new connection to #{message}"
-        init_subscribe message
-      end
-    end
-  end
-end
-
-def do_publish
   MQTT::Client.connect(connection) do |c|
-    loop do
-      puts "Doing publish to admin@a.com"
-      c.publish('admin@a.com', '0,0,0,0')
-      sleep 5
+    c.get("new_connection") do |topic, message|
+      puts "new connection to #{message}"
+      init_subscribe message
     end
   end
 end
 
 init_connection
-do_publish
--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,3 +1,27 @@
 class Measurement < ActiveRecord::Base
   belongs_to :user
+
+  def oxygen_level
+    if blood_oxygenation <= 30
+      1
+    elsif blood_oxygenation <= 50
+      2
+    elsif blood_oxygenation <= 80
+      3
+    else
+      0
+    end
+  end
+
+  def pulse_level
+    if pulse_rate >= 160 or pulse_rate <= 30
+      1
+    elsif pulse_rate >= 140 or pulse_rate <= 40
+      2
+    elsif pulse_rate >= 120 or pulse_rate <= 60
+      3
+    else
+      0
+    end
+  end  
 end
